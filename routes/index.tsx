282d4058d11c1b46645b4fba8787ce42ffc3ab68
CowCowFuture <82391199+CowCowFuture@users.noreply.github.com>
2022-07-16 14:29:05 -0500

    Added links & Header

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,20 +1,29 @@
 /** @jsx h */
 import { h } from "preact";
-import Counter from "../islands/Counter.tsx";
+
+export const Head = () => (
+  <head>
+    <meta charSet="utf-8"/>
+    <title>ccf site</title>
+    <link rel="stylesheet" href="/style.css"/>
+  </head>
+)
+
+export const Links = () => (
+  <div>
+    <p>osu! <a href="https://osu.ppy.sh/users/28964454">CowCowFuture</a></p>
+    <p>github: <a href="https://www.github.com/CowCowFuture/">CowCowFuture</a></p>
+    <p>reddit: <a href="https://www.reddit.com/u/CowCowFuture/">/u/CowCowFuture/</a></p>
+    <p>twitter: <a href="https://youtu.be/dQw4w9WgXcQ">@CowCowFuture</a></p>
+  </div>
+)
 
 export default function Home() {
   return (
     <div>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+      <Head/>
+      <h1>ccf site</h1>
+      <Links/>
     </div>
   );
 }
